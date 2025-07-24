acb74668d8ac5f0989004039890278dd816d3290
rujal408 <rujal408@gmail.com>
2025-07-24 09:44:24 +0545

    refactor

--- applications/project_1/src/pages/field-array-modal/index.tsx
+++ applications/project_1/src/pages/field-array-modal/index.tsx
@@ -89,7 +89,7 @@ const FieldArrayModal: React.FC = () => {
           TabularModalField
         }}
         templates={{
-          TabularModalTemplate: TabularModalTemplate
+          TabularModalTemplate
         }}
       />
     </div>
